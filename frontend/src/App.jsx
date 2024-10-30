fb03896b09d7586bf3379dceb5f6f01e7093c90b
Udayraj <110400788+UdayrajJhala@users.noreply.github.com>
2024-10-30 22:11:44 +0530

    connect todo and taskboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import ToDo from './pages/ToDo/ToDo'
-
-function App() {
-
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ToDo from "./pages/ToDo";
+import Board from "./pages/taskboard";
+const App = () => {
   return (
-    <>
-      <ToDo/>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<ToDo />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
