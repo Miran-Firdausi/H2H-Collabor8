7ec76566c3d5641957d75a0a03270aa1f70ce819
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-11-02 21:25:58 +0530

    included landing page
    minor-changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ToDo from "./pages/ToDo";
 import Board from "./pages/taskboard";
 import GitHubDashboard from "./pages/GithubDashboard";
+import LandingPage from "./pages/LandingPage";
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ToDo />} />
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/todo" element={<ToDo />} />
         <Route path="/board" element={<Board />} />
-        <Route path="/gitboard" element={<GitHubDashboard/>} />
+        <Route path="/gitboard" element={<GitHubDashboard />} />
       </Routes>
     </Router>
   );
