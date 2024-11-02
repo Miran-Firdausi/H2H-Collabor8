4f519a12c63d63754ca72391b722692e34cf2e06
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-11-02 21:03:41 +0530

    added task dependency and few github functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ToDo from "./pages/ToDo";
 import Board from "./pages/taskboard";
+import GitHubDashboard from "./pages/GithubDashboard";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ToDo />} />
         <Route path="/board" element={<Board />} />
+        <Route path="/gitboard" element={<GitHubDashboard/>} />
       </Routes>
     </Router>
   );
