100f2d8ed092a6de8ecdc1a846fa7fce852b2914
Miran <miranfirdausi027@gmail.com>
2024-11-03 22:19:08 +0530

    add projects models and fix calendar bug

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Login from "../pages/Login";
+// import Signup from "../pages/Signup";
+// import Header from "../components/Commons/Header";
+import LandingPage from "../pages/LandingPage";
+import ToDo from "../pages/ToDo";
+import Board from "../pages/taskboard";
+import GitHubDashboard from "../pages/GithubDashboard";
+import Calendar from "../pages/Calendar";
+import PrivateRoute from "./PrivateRoute";
+import Navbar from "../components/Navbar";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<LandingPage />} />
+        <Route path="/todo" element={<PrivateRoute element={ToDo} />} />
+        <Route path="/board" element={<PrivateRoute element={Board} />} />
+        <Route
+          path="/gitboard"
+          element={<PrivateRoute element={GitHubDashboard} />}
+        />
+        <Route path="/calendar" element={<PrivateRoute element={Calendar} />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
