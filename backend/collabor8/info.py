93d0279eb69a048cc8905f4659b7e17c1fc01106
Srishti Parulekar <srishtiparulekar430@gmail.com>
2024-11-06 10:02:29 +0530

    made some changes

--- backend/collabor8/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'help.collabor8@gmail.com'
-EMAIL_HOST_PASSWORD = 'hack2hire'
-EMAIL_PORT = 587
--- backend/collabor8/settings.py
+++ backend/collabor8/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+from dotenv import load_dotenv
+load_dotenv()
 
 import os
 from pathlib import Path
@@ -94,7 +96,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "help.collabor8@gmail.com"
-EMAIL_HOST_PASSWORD = "ucls ajat xbpz dphv"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 # Password validation
